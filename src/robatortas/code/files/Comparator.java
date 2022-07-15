f9d1643f3c13ebf8332010617a547308229fbc82
Robatortas <robatortas1@gmail.com>
2022-07-15 07:30:45 -0800

    Final!

--- src/robatortas/code/files/Comparator.java
+++ src/robatortas/code/files/Comparator.java
@@ -39,8 +39,7 @@ public class Comparator {
 			case 'y': 
 				System.out.println("Lets continue then!\n");
 				System.out.println("Starting comparator... (Let me handle this now!)\n");
-//				compares();
-				compareTest();
+				compares();
 				break;
 			case 'n': System.out.println("ABORTING"); System.exit(0);
 			break;
@@ -51,7 +50,7 @@ public class Comparator {
 			System.exit(0);
 		}
 		
-		System.out.println("*************\nComparation is done!\n\nYour comparated images are processed and in the same folder location.\n\n"
+		System.out.println("\nComparation is done!\n\nYour comparated images are processed and in the same folder location.\n\n"
 				+ "Thank you for using Image Comparator and have a wonderful day!");
 	}
 	
@@ -75,82 +74,8 @@ public class Comparator {
 			int[] comparedPixels = null;
 			
 			String[] filesInDir = new File(dir).list();
-			for(int i = 0; i < fileSize; i++) {
-				System.out.println("COMPARING ====> " + filesInDir[i]);
-				// Without the getClass stuff, it gets files from the whole drive, not just from the resources.
-				image = ImageIO.read(new File(dir + "\\" + filesInDir[i]));
-				int w = image.getWidth();
-				int h = image.getHeight();
-				pixels = new int[w*h];
-				image.getRGB(0, 0, w, h, pixels, 0, w);
-				
-				comparedImage = ImageIO.read(new File(dir + "\\" + filesInDir[i]));
-				int ww = comparedImage.getWidth();
-				int hh = comparedImage.getHeight();
-				comparedPixels = new int[ww*hh];
-				comparedImage.getRGB(0, 0, ww, hh, comparedPixels, 0, ww);
-				
-				int RGB = 0;
-				int comparedRGB = 0;
-				
-				// I am separating pixels just in case we might use it for the future!
-				// And I know I could just assigne image.getRGB() to a variable, but it's better this way for future use.
-				
-				for(int j = 0; j < pixels.length; j++) {
-					int a = (pixels[i] & 0xff000000) >> 24;
-					int r = (pixels[i] & 0xff0000) >> 16;
-					int g = (pixels[i] & 0xff00) >> 8;
-					int b = (pixels[i] & 0xff);
-					
-					RGB = a << 24 | r << 16 | g << 8 | b;
-				}
-				
-				for(int j = 0; j < comparedPixels.length; j++) {
-					int a = (comparedPixels[i] & 0xff000000) >> 24;
-					int r = (comparedPixels[i] & 0xff0000) >> 16;
-					int g = (comparedPixels[i] & 0xff00) >> 8;
-					int b = (comparedPixels[i] & 0xff);
-					
-					comparedRGB = a << 24 | r << 16 | g << 8 | b;
-				}
-				
-				if(RGB == comparedRGB && w*h == ww*hh) {
-					System.err.println("Match");
-				} else {
-					System.out.println("Pass");
-				}
-				
-			}
-		} catch (IOException e) {
-			System.out.println("\n");
-			System.err.println(e.getMessage());
-			System.out.println("ABORTING");
-			System.exit(0);
-		}
-	}
-	
-	public void compareTest() {
-		String dots = "comparing...";
-		for(int i = 0; i < dots.length(); i++) {
-			try {
-				Thread.sleep(100);
-				System.out.print(dots.charAt(i));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.print("\n\n");
-		
-		try {
-			int fileSize = new File(dir).list().length;
-			System.out.println("Num of files: " + fileSize + "\n***************");
 			
-			int[] pixels = null;
-			int[] comparedPixels = null;
-			
-			String[] filesInDir = new File(dir).list();
-			
-			int originalFile = 1;
+			int originalFile = 0;
 			
 //			for(int k = 0; k < fileSize; k++)
 			while(originalFile < fileSize) {
@@ -195,14 +120,17 @@ public class Comparator {
 				
 				if(RGB == comparedRGB && w*h == ww*hh && i != originalFile) {
 					System.err.println("Match");
+					new File(dir + "\\" + filesInDir[originalFile]).delete();
 				} else {
 					System.out.println("Pass");
 				}
 				
-				if(i == fileSize-1 && originalFile <= fileSize) originalFile++;
+				if(i == fileSize-1 && originalFile <= fileSize) {
+					originalFile++;
+					System.out.println("*************");
+				}
 				
 				Thread.sleep(2);
-				System.out.println("ORIGINAL FILE VALUE = " + originalFile);
 			}
 			}
 		} catch (IOException e) {
