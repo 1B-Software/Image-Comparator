8cdc4e280742cedaa7cb2028b762b859b3eb5eda
Robatortas <robatortas1@gmail.com>
2022-07-15 07:11:21 -0800

    Comparator works manually!

--- src/robatortas/code/files/Comparator.java
+++ src/robatortas/code/files/Comparator.java
@@ -24,11 +24,10 @@ public class Comparator {
 	}
 	
 	public void console() {
-		System.out.println("WELCOME TO IMAGE COMPARATOR\n\n" + 
+		System.out.println("\nWELCOME TO IMAGE COMPARATOR by Robatortas\n\n" + 
 				"This program is for comparing images and deleting the doppleganger images, only leaving one image\n"
-				+ "This program is specifically used for some stuff I am working on, it is not intended for public use.\n\n"
-				+ "If you want to use this program, suit yourself, this code is open source so if you want to use it you can! ;)\n\n"
-				+ "-Made by Robatortas");
+				+ "This is specifically used for some stuff I am working on, it is not intended for public use.\n\n"
+				+ "If you want to use this program, suit yourself, this code is open source so if you want to use it you can! ;)\n\n");
 		
 		System.out.println("Input the directory where you want to compare your images (I can wait...): ");
 		this.dir = readInput.nextLine().toLowerCase();
@@ -39,17 +38,21 @@ public class Comparator {
 			switch(correct.charAt(0)) {
 			case 'y': 
 				System.out.println("Lets continue then!\n");
-				System.out.println("Starting comparator...\n");
-				compares();
+				System.out.println("Starting comparator... (Let me handle this now!)\n");
+//				compares();
+				compareTest();
 				break;
-			case 'n': System.out.println("Aborting..."); System.exit(0);
+			case 'n': System.out.println("ABORTING"); System.exit(0);
 			break;
 			}
 		} catch(IndexOutOfBoundsException e) {
 			System.err.println(e.getMessage());
-			System.out.println("ABORTING");
+			e.printStackTrace();
 			System.exit(0);
 		}
+		
+		System.out.println("*************\nComparation is done!\n\nYour comparated images are processed and in the same folder location.\n\n"
+				+ "Thank you for using Image Comparator and have a wonderful day!");
 	}
 	
 	public void compares() {
@@ -66,7 +69,7 @@ public class Comparator {
 		
 		try {
 			int fileSize = new File(dir).list().length;
-			System.out.println("Num of files: " + fileSize);
+			System.out.println("Num of files: " + fileSize + "\n***************");
 			
 			int[] pixels = null;
 			int[] comparedPixels = null;
@@ -82,11 +85,10 @@ public class Comparator {
 				image.getRGB(0, 0, w, h, pixels, 0, w);
 				
 				comparedImage = ImageIO.read(new File(dir + "\\" + filesInDir[i]));
-				int ww = image.getWidth();
-				int hh = image.getHeight();
+				int ww = comparedImage.getWidth();
+				int hh = comparedImage.getHeight();
 				comparedPixels = new int[ww*hh];
-				image.getRGB(0, 0, ww, hh, comparedPixels, 0, ww);
-				
+				comparedImage.getRGB(0, 0, ww, hh, comparedPixels, 0, ww);
 				
 				int RGB = 0;
 				int comparedRGB = 0;
@@ -94,7 +96,7 @@ public class Comparator {
 				// I am separating pixels just in case we might use it for the future!
 				// And I know I could just assigne image.getRGB() to a variable, but it's better this way for future use.
 				
-				for(int j = 0; j < pixels.length; i++) {
+				for(int j = 0; j < pixels.length; j++) {
 					int a = (pixels[i] & 0xff000000) >> 24;
 					int r = (pixels[i] & 0xff0000) >> 16;
 					int g = (pixels[i] & 0xff00) >> 8;
@@ -103,7 +105,7 @@ public class Comparator {
 					RGB = a << 24 | r << 16 | g << 8 | b;
 				}
 				
-				for(int j = 0; j < comparedPixels.length; i++) {
+				for(int j = 0; j < comparedPixels.length; j++) {
 					int a = (comparedPixels[i] & 0xff000000) >> 24;
 					int r = (comparedPixels[i] & 0xff0000) >> 16;
 					int g = (comparedPixels[i] & 0xff00) >> 8;
@@ -120,6 +122,92 @@ public class Comparator {
 				
 			}
 		} catch (IOException e) {
+			System.out.println("\n");
+			System.err.println(e.getMessage());
+			System.out.println("ABORTING");
+			System.exit(0);
+		}
+	}
+	
+	public void compareTest() {
+		String dots = "comparing...";
+		for(int i = 0; i < dots.length(); i++) {
+			try {
+				Thread.sleep(100);
+				System.out.print(dots.charAt(i));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.print("\n\n");
+		
+		try {
+			int fileSize = new File(dir).list().length;
+			System.out.println("Num of files: " + fileSize + "\n***************");
+			
+			int[] pixels = null;
+			int[] comparedPixels = null;
+			
+			String[] filesInDir = new File(dir).list();
+			
+			int originalFile = 1;
+			
+//			for(int k = 0; k < fileSize; k++)
+			for(int i = 0; i < fileSize; i++) {
+				System.out.println("COMPARING ====> " + filesInDir[i]);
+				// Without the getClass stuff, it gets files from the whole drive, not just from the resources.
+				image = ImageIO.read(new File(dir + "\\" + filesInDir[originalFile]));
+				int w = image.getWidth();
+				int h = image.getHeight();
+				pixels = new int[w*h];
+				image.getRGB(0, 0, w, h, pixels, 0, w);
+				
+				comparedImage = ImageIO.read(new File(dir + "\\" + filesInDir[i]));
+				int ww = comparedImage.getWidth();
+				int hh = comparedImage.getHeight();
+				comparedPixels = new int[ww*hh];
+				comparedImage.getRGB(0, 0, ww, hh, comparedPixels, 0, ww);
+				
+				int RGB = 0;
+				int comparedRGB = 0;
+				
+				// I am separating pixels just in case we might use it for the future!
+				// And I know I could just assign image.getRGB() to a variable, but it's better this way for future use.
+				
+				for(int j = 0; j < pixels.length; j++) {
+					int a = (pixels[j] & 0xff000000) >> 24;
+					int r = (pixels[j] & 0xff0000) >> 16;
+					int g = (pixels[j] & 0xff00) >> 8;
+					int b = (pixels[j] & 0xff);
+					
+					RGB = a << 24 | r << 16 | g << 8 | b;
+				}
+				
+				for(int j = 0; j < comparedPixels.length; j++) {
+					int a = (comparedPixels[j] & 0xff000000) >> 24;
+					int r = (comparedPixels[j] & 0xff0000) >> 16;
+					int g = (comparedPixels[j] & 0xff00) >> 8;
+					int b = (comparedPixels[j] & 0xff);
+					
+					comparedRGB = a << 24 | r << 16 | g << 8 | b;
+				}
+				
+				if(RGB == comparedRGB && w*h == ww*hh) {
+					System.err.println("Match");
+				} else {
+					System.out.println("Pass");
+				}
+				
+				Thread.sleep(10);
+				
+			}
+		} catch (IOException e) {
+			System.out.println("\n");
+			System.err.println(e.getMessage());
+			System.out.println("ABORTING");
+			System.exit(0);
+		} catch (InterruptedException e) {
+			System.out.println("\n");
 			System.err.println(e.getMessage());
 			System.out.println("ABORTING");
 			System.exit(0);
